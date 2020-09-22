d1a29ba25bb9aed9f1cfac1dd5853ea3b79d517d
Rahul Shah <shahdotrahul@gmail.com>
2020-09-22 20:37:57 +0100

    added option to have an image with a post

---
+++ mysite/settings/dev.py
@@ -0,0 +1,16 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '_5=#=+cl&lp@&ayps6ia0viff)^v$_wvutyyxca!xu0w6d2z3$'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
