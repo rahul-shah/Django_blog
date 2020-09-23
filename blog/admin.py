be2eb9c60a2d47bb842b6cb0f3b983c8d5dbf0a6
Rahul Shah <shahdotrahul@gmail.com>
2020-09-23 22:36:22 +0100

    circleci config added

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post,PostImage
 from django.db import models
 
-admin.site.register(Post)
+class PostImageAdmin(admin.StackedInline):
+    model = PostImage
+ 
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [PostImageAdmin]
+ 
+    class Meta:
+       model = Post
+ 
+@admin.register(PostImage)
+class PostImageAdmin(admin.ModelAdmin):
+    pass
--- blog/models.py
+++ blog/models.py
@@ -7,8 +7,13 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete= models.CASCADE)
     title = models.CharField(max_length = 50000)
     text = HTMLField('Content')
+    image = models.FileField(blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+
+class PostImage(models.Model):
+    post = models.ForeignKey(Post, default=None, on_delete=models.CASCADE)
+    images = models.FileField(upload_to = 'images/')
 
 def publish(self):
     self.published_date = timezone.now
