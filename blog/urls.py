bd623ac6314bdc9b79a8ac42bfb59e2603c50e75
Rahul Shah <shahdotrahul@gmail.com>
2020-09-26 22:19:41 +0100

    rewired the blog UI

--- blog/urls.py
+++ blog/urls.py
@@ -14,9 +14,11 @@ from django.views.generic import TemplateView
 from .views import HomeView
 
 urlpatterns = [
-    path('home/', HomeView.as_view(), name='home_url'),
-    path('', TemplateView.as_view(template_name='blog/index.html'), name='index_url'),
+    # path('home/', HomeView.as_view(), name='home_url'),
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    # path('', TemplateView.as_view(template_name='blog/index.html'), name='index_url'),
     path('about/', TemplateView.as_view(template_name='blog/about.html'), name='about_url'),
     path('contact/', TemplateView.as_view(template_name='blog/contact.html'), name='contact_url'),
-    path('post/', TemplateView.as_view(template_name='blog/post.html'), name='post_url'),
+    # path('post/', TemplateView.as_view(template_name='blog/post.html'), name='post_url'),
 ]
